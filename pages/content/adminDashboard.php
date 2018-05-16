84939aa38e7a968b3fa4a3f9ebdbac6bb33cfeca
dewasugiarta <35513925+dewasugiarta@users.noreply.github.com>
2018-05-16 18:28:17 +0800

    nothing

--- pages/content/adminDashboard.php
+++ pages/content/adminDashboard.php
@@ -13,14 +13,14 @@
             </a>
         </div>
         <div class="col-md-4" style="padding:10px;">
-            <a class="btn btn-app" style="height:200px;width:100%; padding:30px;background:#fab005;">
-                <i class="fa fa-sign-out" style="font-size:80px;color:white;"></i>
+            <a href="index.php?pageid=opd" class="btn btn-app" style="height:200px;width:100%; padding:30px;background:#4de6ca;">
+                <i class="fa fa-bank" style="font-size:80px;color:white;"></i>
                 <p style="font-size:25px;color:white;">OPD</p>
             </a>
         </div>
         <div class="col-md-4" style="padding:10px;">
-            <a class="btn btn-app" style="height:200px;width:100%; padding:30px;background:#fab005;">
-                <i class="fa fa-sign-out" style="font-size:80px;color:white;"></i>
+            <a class="btn btn-app" style="height:200px;width:100%; padding:30px;background:#ff4c52;">
+                <i class="fa fa-user" style="font-size:80px;color:white;"></i>
                 <p style="font-size:25px;color:white;">USER</p>
             </a>
         </div>
