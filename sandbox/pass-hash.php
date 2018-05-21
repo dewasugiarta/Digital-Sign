5ea8adfb2ff64c1f4b39d66de395009daf9ea49c
Sukma Raharja <sukmaraharja21@gmail.com>
2018-05-21 09:25:11 +0800

    update

--- sandbox/pass-hash.php
+++ sandbox/pass-hash.php
@@ -1,13 +1,11 @@
 <?php
 
 
-$pass = 'stikombali';
+$pass = 'sukma';
 $options = array(
     'cost'=>12,
     'salt'=>uniqid(mt_rand(), true)
 );
 
 $newPass = password_hash($pass, PASSWORD_BCRYPT, $options);
-var_dump($newPass);
-
-echo password_verify('stikombali', $newPass)?"success":"failed";
+echo $newPass;
